82af46fed6bb30ab93d7480c4bd0665c476d0da7
Azwin-1997 <125340831+Azwin-1997@users.noreply.github.com>
2024-08-30 18:27:46 +0530

    Time: 74 ms (74.6%), Space: 57.9 MB (12.27%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.js
@@ -0,0 +1,9 @@
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function(s) {
+    let x = s.reverse()
+    return x
+};
+console.log(reverseString(["h","e","l","l","o"]))
