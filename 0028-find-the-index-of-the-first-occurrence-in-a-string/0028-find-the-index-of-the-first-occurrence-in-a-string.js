4d1539dabf8bf0e00bdae43b00469867eb0c52e1
Azwin-1997 <125340831+Azwin-1997@users.noreply.github.com>
2024-09-06 12:40:26 +0530

    Time: 55 ms (41.23%), Space: 48.6 MB (79.87%) - LeetHub

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
@@ -0,0 +1,10 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+    let inc = haystack.indexOf(needle)
+    return inc
+    
+};
