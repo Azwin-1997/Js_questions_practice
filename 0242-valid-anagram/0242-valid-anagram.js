96645fa7bff71fc48c7d99fed22a1925cb8a8711
Azwin-1997 <125340831+Azwin-1997@users.noreply.github.com>
2025-02-18 11:35:19 +0530

    Time: 30 ms (23.66%), Space: 54.5 MB (24.11%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -3,9 +3,17 @@
  * @param {string} t
  * @return {boolean}
  */
-function isAnagram(s, t) {
-    if (s.length !== t.length) {
-        return false;
+var isAnagram = function(s, t) {
+    
+    if(s.length != t.length){
+        return false
     }
-        return s.split('').sort().join('') === t.split('').sort().join('');
-}
+    
+    let s1 = s.split("").sort().join()
+    let t1 = t.split("").sort().join()
+   if(s1 === t1){
+       return true
+   }else{
+       return false
+   }
+};
