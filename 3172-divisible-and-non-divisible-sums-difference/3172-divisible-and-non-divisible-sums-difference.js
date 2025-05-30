8ccdc38adc3550c556a3b0a4e72cd638b0dc0adf
Azwin-1997 <125340831+Azwin-1997@users.noreply.github.com>
2025-05-30 10:22:47 +0530

    Time: 0 ms (100%), Space: 53.5 MB (94.87%) - LeetHub

--- 3172-divisible-and-non-divisible-sums-difference/3172-divisible-and-non-divisible-sums-difference.js
+++ 3172-divisible-and-non-divisible-sums-difference/3172-divisible-and-non-divisible-sums-difference.js
@@ -1,26 +1,20 @@
+/**
+ * @param {number} n
+ * @param {number} m
+ * @return {number}
+ */
 var differenceOfSums = function(n, m) {
-    var divisibleArr = []
-    var nonDivisibleArr = []
+let divSum = 0
+let notDivSum = 0
+
     for(i=1;i<=n;i++){
-        if(i%m!==0){
-            divisibleArr.push(i)
+        if(i%m==0){
+            divSum+=i
         }else{
-            nonDivisibleArr.push(i)
+            notDivSum+=i
         }
-        
+       var sumDiff = notDivSum - divSum
     }
-   
-    var newArr1 = divisibleArr.reduce((acc,curr)=>{
-        return acc+curr
-    },0)
-    
-    var newArr2 = nonDivisibleArr.reduce((acc,curr)=>{
-        return acc+curr
-    },0)
-    
-    var diff = newArr1-newArr2
-    return diff
-     
+   return sumDiff
     
 };
-console.log(differenceOfSums(10,3))
