f09507433c0cb3317f391b87bed3867ce0a86ab4
Azwin-1997 <125340831+Azwin-1997@users.noreply.github.com>
2025-12-06 10:28:37 +0530

    Time: 1 ms (67.74%), Space: 56.7 MB (37.1%) - LeetHub

---
+++ 3432-count-partitions-with-even-sum-difference/3432-count-partitions-with-even-sum-difference.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var countPartitions = function(nums) {
+    let total = 0;
+
+    
+    for (let i = 0; i < nums.length; i++) {
+        total += nums[i];
+    }
+
+    let leftSum = 0;
+    let count = 0;
+
+      for (let i = 0; i < nums.length - 1; i++) {
+
+        leftSum += nums[i];
+        let rightSum = total - leftSum;
+
+        let diff = leftSum - rightSum;
+
+        if (diff % 2 === 0) {
+            count++;
+        }
+    }
+
+    return count;
+};
